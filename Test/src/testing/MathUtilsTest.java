ae4ca3e9ff262b5ad4d3cc1a46422a3226cfd35a
Nihal <95596541+Nihal2999@users.noreply.github.com>
2022-06-28 21:19:30 +0530

    Add files via upload

--- Test/src/testing/MathUtilsTest.java
+++ Test/src/testing/MathUtilsTest.java
@@ -3,18 +3,24 @@ package testing;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("When running MathUtils")
 class MathUtilsTest {
 	
 	MathUtils mathUtils;
+	TestInfo testInfo;
+	TestReporter testReporter;
 	
 	/*@BeforeAll
 	static void beforeAllInit() {		
@@ -22,8 +28,11 @@ class MathUtilsTest {
 	}*/
 	
 	@BeforeEach
-	void init() {	
-		mathUtils = new MathUtils();	
+	void init(TestInfo testInfo, TestReporter testReporter) {
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
+		mathUtils = new MathUtils();
+		testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags" + testInfo.getTags());
 	}
 	
 	/*@AfterAll
@@ -38,6 +47,7 @@ class MathUtilsTest {
 
 	@Nested
 	@DisplayName("add method")
+	@Tag("Math")
 	class AddTest {
 		
 		@Test
@@ -50,14 +60,18 @@ class MathUtilsTest {
 		
 		@Test
 		@DisplayName("when adding two negative numbers")
-		void testAddNegative() {	
-			assertEquals(-2, mathUtils.add(-1, -1), "Should return the right Sum" );
+		void testAddNegative() {
+			int expected = -2;
+			int actual = mathUtils.add(-1, -1);
+			assertEquals(expected, actual, () -> "Should return  Sum" + expected + "but returned" + actual);
 		}
 	}
 	
 	@Test
 	@DisplayName("multiply method")
+	@Tag("Math")
 	void testMultiply() {
+	testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags" + testInfo.getTags());
 	//assertEquals(4, mathUtils.multiply(2, 2), "Should return the right Product" );
 	assertAll(
 			() -> assertEquals(4, mathUtils.multiply(2, 2)),
@@ -68,16 +82,18 @@ class MathUtilsTest {
 	
 	@Test
 	//@EnabledOnOs(OS.LINUX)
-	@DisplayName("Testing divide method")
+	@DisplayName("divide method")
+	@Tag("Math")
 	void testDivide() {
-		Boolean isServerUp = false;
-		assumeTrue(isServerUp);
+		//Boolean isServerUp = false;
+		//assumeTrue(isServerUp);
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");	
 	}
 	
-	@Test
+	@RepeatedTest(3)
 	@DisplayName("Testing ComputeCircleRadius method")
-	void testComputeCircleRadius() {
+	@Tag("Circle")
+	void testComputeCircleRadius(RepetitionInfo repetitionInfo) {
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area" );
 	}
 	
